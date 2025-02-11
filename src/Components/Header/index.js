ab0819af15c0bc91aa7ed53cedc3302bfc66e9a0
Vishvam <vishusp77@gmail.com>
2025-02-11 12:27:36 +0530

    update

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,12 +1,13 @@
 import { Link } from 'react-router-dom';
-import Logo from '../../assets/images/logo.jpeg'
+import Logo from '../../assets/images/logo.jpg'
 import CountryDropdown from '../CountryDropdown';
 import Button from '@mui/material/Button';
-import { IoSearch } from "react-icons/io5";
+import { IoIosSearch } from "react-icons/io";
+import { FiUser } from "react-icons/fi";
+import { IoBagOutline } from "react-icons/io5";
 
-
-const Header= ()=>{
-    return(
+const Header = () => {
+    return (
         <>
             <div className="headerWrapper">
                 <div className="top-strip bg-blue">
@@ -26,12 +27,27 @@ const Header= ()=>{
                             <div className='col-sm-10 d-flex align-items-center part2'>
                                 <CountryDropdown/>
 
-                                {/* Header Search Start here*/}
-                                    <div className ='headerSearch ml-3 mr-3'>
-                                        <input type='text'/>
-                                        <Button><IoSearch/></Button>
-                                    </div>    
-                                {/* Header Search End here*/}
+                                {/* Header Search Start here */}
+                                <div className='headerSearch ml-3 mr-3'>
+                                    <input type='text' placeholder='Search for products...' />
+                                    <Button><IoIosSearch /></Button>
+                                </div>    
+                                {/* Header Search End here */}
+
+                                <div className='part3 d-flex align-items-center ml-auto'>
+                                    <Button className='circle mr-3'>
+                                        <FiUser />
+                                    </Button>
+                                    <div className='ml-auto cartTab d-flex align-items-center'>
+                                        <span className='price'>$3.29</span>
+                                        <div className='position-relative ml-2'>
+                                            <Button className='circle'>
+                                                <IoBagOutline />
+                                            </Button>
+                                            <span className='count d-flex align-items-center justify-content-center'>1</span>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
