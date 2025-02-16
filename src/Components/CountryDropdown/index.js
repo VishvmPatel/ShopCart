a1e03c502580f315df30646925f35d5e731f7749
Vishvam <vishusp77@gmail.com>
2025-02-16 18:48:39 +0530

    update

--- src/Components/CountryDropdown/index.js
+++ src/Components/CountryDropdown/index.js
@@ -1,5 +1,7 @@
 import Button from '@mui/material/Button';
 import { FaAngleDown } from "react-icons/fa6";
+import Dialog from '@mui/material/Dialog';
+import { IoIosSearch } from "react-icons/io";
 
 const CountryDropdown=()=>{
     return(
@@ -11,6 +13,47 @@ const CountryDropdown=()=>{
                 </div>
                 <span className='ml-auto'><FaAngleDown/></span>
             </Button>
+
+            <Dialog open={true} className='locationModel'>
+                <h4>Choose your Delievery Location</h4>  
+                <p>Enter your address and we will specify the offer for your area.</p>  
+                <div className='headerSearch w-100'>
+                    <input type='text' placeholder='Search your area...' />
+                    <Button><IoIosSearch /></Button>
+                </div>  
+                <ul className='countryList mt-3'>
+                    <li><Button>India</Button></li>
+                    <li><Button>Sri Lanka</Button></li>
+                    <li><Button>Pakistan</Button></li>
+                    <li><Button>India</Button></li>
+                    <li><Button>Sri Lanka</Button></li>
+                    <li><Button>Pakistan</Button></li>
+                    <li><Button>India</Button></li>
+                    <li><Button>Sri Lanka</Button></li>
+                    <li><Button>Pakistan</Button></li>
+                    <li><Button>India</Button></li>
+                    <li><Button>Sri Lanka</Button></li>
+                    <li><Button>Pakistan</Button></li>
+                    <li><Button>India</Button></li>
+                    <li><Button>Sri Lanka</Button></li>
+                    <li><Button>Pakistan</Button></li>
+                    <li><Button>India</Button></li>
+                    <li><Button>Sri Lanka</Button></li>
+                    <li><Button>Pakistan</Button></li>
+                    <li><Button>India</Button></li>
+                    <li><Button>Sri Lanka</Button></li>
+                    <li><Button>Pakistan</Button></li>
+                    <li><Button>India</Button></li>
+                    <li><Button>Sri Lanka</Button></li>
+                    <li><Button>Pakistan</Button></li>
+                    <li><Button>India</Button></li>
+                    <li><Button>Sri Lanka</Button></li>
+                    <li><Button>Pakistan</Button></li>
+                    <li><Button>India</Button></li>
+                    <li><Button>Sri Lanka</Button></li>
+                    <li><Button>Pakistan</Button></li>
+                </ul>       
+            </Dialog>
         </>
     )
 }
