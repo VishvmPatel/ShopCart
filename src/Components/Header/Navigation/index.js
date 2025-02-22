1165f5b88ea10bef242a389f4e63529502718942
Vishvam <vishusp77@gmail.com>
2025-02-22 21:21:27 +0530

    update

--- src/Components/Header/Navigation/index.js
+++ src/Components/Header/Navigation/index.js
@@ -2,18 +2,39 @@ import Button from '@mui/material/Button';
 import { IoIosMenu } from "react-icons/io";
 import { FaAngleDown } from "react-icons/fa6";
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const Navigation = () =>{
+
+    const [isOpenSidebarVal , setisopenSidebarVal] = useState(true);
     return(
         <nav>
             <div className='container'>
                 <div className='row'>
                     <div className='col-sm-2 navPart1'>
-                        <Button className='allCatTab align-items-center'>
-                            <span className='icon1 mr-2'><IoIosMenu/></span>
-                            <span class="text">ALL CATEGORIES</span>
-                            <span className='icon2 ml-2'><FaAngleDown/></span>
-                        </Button>
+                        <div className='catWrapper'>
+                            <Button className='allCatTab align-items-center' onClick={()=>setisopenSidebarVal(!isOpenSidebarVal)}>
+                                <span className='icon1 mr-2'><IoIosMenu/></span>
+                                <span class="text">ALL CATEGORIES</span>
+                                <span className='icon2 ml-2'><FaAngleDown/></span>
+                            </Button>
+                            <div className={`sidebarNav ${isOpenSidebarVal===true ? 'open' : ''}`}> 
+                                <ul>
+                                    <li><Link to="/"><Button>men</Button></Link></li>
+                                    <li><Link to="/"><Button>women</Button></Link></li>
+                                    <li><Link to="/"><Button>beauty</Button></Link></li>
+                                    <li><Link to="/"><Button>watches</Button></Link></li>
+                                    <li><Link to="/"><Button>kids</Button></Link></li>
+                                    <li><Link to="/"><Button>gifts</Button></Link></li>
+                                    <li><Link to="/"><Button>men</Button></Link></li>
+                                    <li><Link to="/"><Button>women</Button></Link></li>
+                                    <li><Link to="/"><Button>beauty</Button></Link></li>
+                                    <li><Link to="/"><Button>watches</Button></Link></li>
+                                    <li><Link to="/"><Button>kids</Button></Link></li>
+                                    <li><Link to="/"><Button>gifts</Button></Link></li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
 
                     <div className='col-sm-10 navPart2 d-flex align-items-center'>
