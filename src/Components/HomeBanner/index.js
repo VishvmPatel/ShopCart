50ce16202644418e1f3143574b17b26221a25665
Vishvam <vishusp77@gmail.com>
2025-02-25 09:58:54 +0530

    update

--- src/Components/HomeBanner/index.js
+++ src/Components/HomeBanner/index.js
@@ -3,17 +3,27 @@ import Slider from "react-slick";
 
 const HomeBanner = ()=>{
     var settings = {
-        dots: true,
+        dots: false,
         infinite: true,
         speed: 500,
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        arrows: true
     };
     return(
         <div className="homeBannerSection">
             <Slider {...settings}>
                 <div className="item">
-                    <img src="" className="w-100"/>
+                    <img src="https://cmsimages.shoppersstop.com/SS_25_main_kv_web_fd8e548010/SS_25_main_kv_web_fd8e548010.png" className="w-100"/>
+                </div>
+                <div className="item">
+                    <img src="https://cmsimages.shoppersstop.com/SS_25_pvt_brands_main_kv_web_e55ed30937/SS_25_pvt_brands_main_kv_web_e55ed30937.png" className="w-100"/>
+                </div>
+                <div className="item">
+                    <img src="https://cmsimages.shoppersstop.com/WW_Main_Banners_web_98669c5889/WW_Main_Banners_web_98669c5889.png" className="w-100"/>
+                </div>
+                <div className="item">
+                    <img src="https://cmsimages.shoppersstop.com/Women_Indian_Main_Banners_web_3466e7b36d/Women_Indian_Main_Banners_web_3466e7b36d.png" className="w-100"/>
                 </div>
             </Slider>  
         </div>  
