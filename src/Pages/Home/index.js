d89ac8fd41e3791c4c3327d19d70e6349f54857d
Vishvam <vishusp77@gmail.com>
2025-03-18 09:40:19 +0530

    update

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -4,6 +4,18 @@ const Home = ()=>{
     return(
         <>
             <HomeBanner/>
+
+            <section className="homeProducts">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-3">
+                            <img src="D:\projectecommerce\src\assets\images\banner1.jpg" className="cursor"></img>
+                        </div>
+                        <div className="col-md-3">
+                        </div>
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
