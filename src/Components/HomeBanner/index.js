08355b5b3275805c8d20e896e856cdb6a97e8ca3
Vishvam <vishusp77@gmail.com>
2025-03-20 22:23:01 +0530

    update

--- src/Components/HomeBanner/index.js
+++ src/Components/HomeBanner/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Slider from "react-slick";
 
-const HomeBanner = ()=>{
+const HomeBanner = () => {
     var settings = {
         dots: false,
         infinite: true,
@@ -15,16 +15,32 @@ const HomeBanner = ()=>{
         <div className="homeBannerSection">
             <Slider {...settings}>
                 <div className="item">
-                    <img src="https://cmsimages.shoppersstop.com/SS_25_main_kv_web_fd8e548010/SS_25_main_kv_web_fd8e548010.png" className="w-100"/>
+                    <img 
+                        src="https://cmsimages.shoppersstop.com/SS_25_main_kv_web_fd8e548010/SS_25_main_kv_web_fd8e548010.png" 
+                        className="w-100"
+                        alt="Shopper's Stop Banner 1"
+                    />
                 </div>
                 <div className="item">
-                    <img src="https://cmsimages.shoppersstop.com/SS_25_pvt_brands_main_kv_web_e55ed30937/SS_25_pvt_brands_main_kv_web_e55ed30937.png" className="w-100"/>
+                    <img 
+                        src="https://cmsimages.shoppersstop.com/SS_25_pvt_brands_main_kv_web_e55ed30937/SS_25_pvt_brands_main_kv_web_e55ed30937.png" 
+                        className="w-100"
+                        alt="Shopper's Stop Private Brands Banner"
+                    />
                 </div>
                 <div className="item">
-                    <img src="https://cmsimages.shoppersstop.com/WW_Main_Banners_web_98669c5889/WW_Main_Banners_web_98669c5889.png" className="w-100"/>
+                    <img 
+                        src="https://cmsimages.shoppersstop.com/WW_Main_Banners_web_98669c5889/WW_Main_Banners_web_98669c5889.png" 
+                        className="w-100"
+                        alt="Women's Wear Banner"
+                    />
                 </div>
                 <div className="item">
-                    <img src="https://cmsimages.shoppersstop.com/Women_Indian_Main_Banners_web_3466e7b36d/Women_Indian_Main_Banners_web_3466e7b36d.png" className="w-100"/>
+                    <img 
+                        src="https://cmsimages.shoppersstop.com/Women_Indian_Main_Banners_web_3466e7b36d/Women_Indian_Main_Banners_web_3466e7b36d.png" 
+                        className="w-100"
+                        alt="Women's Indian Wear Banner"
+                    />
                 </div>
             </Slider>  
         </div>  
--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -4,17 +4,18 @@ import Button from '@mui/material/Button';
 import { IoIosArrowRoundForward } from "react-icons/io";
 import React from "react";
 import Slider from "react-slick";
-
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Home = ()=>{
 
     var productSliderOptions = {
         dots: true,
-        infinite: true,
+        infinite: false,
         speed: 500,
         slidesToShow: 4,
         slidesToScroll: 1,
-        arrows: true
+        arrows: true,
     };
     return(
         <>
@@ -36,9 +37,32 @@ const Home = ()=>{
                                 </div>
                                 <Button className="viewAllBtn ml-auto">View All<IoIosArrowRoundForward/></Button>
                             </div>  
-                            <div className="product_row">
+                            <div className="product_row w-100">
                                 <Slider {...productSliderOptions}>
-                                    <div className="item">      
+                                    <div className="item productItem">   
+                                        <div className="imgWrapper">
+                                            <img src="https://klbtheme.com/bacola/wp-content/uploads/2021/04/product-image-3-346x310.jpg" className="w-100" alt="Product item 1" />
+                                        </div>   
+                                    </div>
+                                    <div className="item productItem">   
+                                        <div className="imgWrapper">
+                                            <img src="https://klbtheme.com/bacola/wp-content/uploads/2021/04/product-image-3-346x310.jpg" className="w-100" alt="Product item 2" />
+                                        </div>   
+                                    </div>
+                                    <div className="item productItem">   
+                                        <div className="imgWrapper">
+                                            <img src="https://klbtheme.com/bacola/wp-content/uploads/2021/04/product-image-3-346x310.jpg" className="w-100" alt="Product item 3" />
+                                        </div>   
+                                    </div>
+                                    <div className="item productItem">   
+                                        <div className="imgWrapper">
+                                            <img src="https://klbtheme.com/bacola/wp-content/uploads/2021/04/product-image-3-346x310.jpg" className="w-100" alt="Product item 4" />
+                                        </div>   
+                                    </div>
+                                    <div className="item productItem">   
+                                        <div className="imgWrapper">
+                                            <img src="https://klbtheme.com/bacola/wp-content/uploads/2021/04/product-image-3-346x310.jpg" className="w-100" alt="Product item 5" />
+                                        </div>   
                                     </div>
                                 </Slider>  
                             </div>
@@ -50,4 +74,4 @@ const Home = ()=>{
     )
 }
 
-export default Home ;
+export default Home;
