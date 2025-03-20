921c15f14acf6650ff42486968736f7d2bd94617
Vishvam <vishusp77@gmail.com>
2025-03-20 19:36:30 +0530

    update

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,5 +1,7 @@
 import HomeBanner from "../../Components/HomeBanner";
-import banner1 from '../../assets/images/banner1.jpg'
+import banner1 from '../../assets/images/banner1.jpg';
+import Button from '@mui/material/Button';
+import { IoIosArrowRoundForward } from "react-icons/io";
 const Home = ()=>{
     return(
         <>
@@ -10,16 +12,17 @@ const Home = ()=>{
                     <div className="row">
                         <div className="col-md-3">
                             <div className="banner">
-                                <img src={banner1} alt="E-commerce Banner" className="cursor" />
+                                <img src={banner1} alt="E-commerce Banner" className="cursor w-100 " />
                             </div>
                         </div>
-                        <div className="col-md-3">
+                        <div className="col-md-9 productRow">
                             <div className="d-flex align-items-center">
                                 <div className="info w-75">
-                                    <h3>BEST SELLERS</h3>
+                                    <h3 className="mb-0 hd">BEST SELLERS</h3>
                                     <p className="text-light text-sml mb-0">Do not miss the current offers untill the end of March.</p>
                                 </div>
-                            </div>
+                                <Button className="viewAllBtn ml-auto">View All<IoIosArrowRoundForward/></Button>
+                            </div>  
                         </div>
                     </div>
                 </div>
