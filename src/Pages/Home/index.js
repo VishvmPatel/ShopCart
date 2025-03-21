38497181b81e53d61022c9ce6cf2b70ab909331c
Vishvam <vishusp77@gmail.com>
2025-03-21 21:10:49 +0530

    update

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -3,9 +3,13 @@ import banner1 from '../../assets/images/banner1.jpg';
 import Button from '@mui/material/Button';
 import { IoIosArrowRoundForward } from "react-icons/io";
 import React from "react";
-import Slider from "react-slick";
+import { Swiper, SwiperSlide } from 'swiper/react';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import 'swiper/css';
+import 'swiper/css/navigation';
+import {Navigation} from 'swiper/modules';
+import Rating from '@mui/material/Rating';
 
 const Home = ()=>{
 
@@ -37,41 +41,54 @@ const Home = ()=>{
                                 </div>
                                 <Button className="viewAllBtn ml-auto">View All<IoIosArrowRoundForward/></Button>
                             </div>  
-                            <div className="product_row w-100">
-                                <Slider {...productSliderOptions}>
+                            <div className="product_row w-100 mt-4">
+                            <Swiper slidesPerView={4} spaceBetween={30} pagination={{ clickable: true,}} modules={[Navigation]} className="mySwiper">
+                                <SwiperSlide>
+                                    <div className="item productItem">   
+                                        <div className="imgWrapper">
+                                            <img src="https://klbtheme.com/bacola/wp-content/uploads/2021/04/product-image-3-346x310.jpg" className="w-100" alt="Product item 1" />
+                                        </div> 
+                                        <h4>Werther’s Original Caramel Hard Candies</h4>  
+                                        <span className="text-success d-block">In Stock</span>
+                                        <Rating name="read-only" value={5} readOnly />
+                                    </div>
+                                </SwiperSlide>
+                                <SwiperSlide>
                                     <div className="item productItem">   
                                         <div className="imgWrapper">
                                             <img src="https://klbtheme.com/bacola/wp-content/uploads/2021/04/product-image-3-346x310.jpg" className="w-100" alt="Product item 1" />
                                         </div>   
                                     </div>
+                                </SwiperSlide>
+                                <SwiperSlide>
                                     <div className="item productItem">   
                                         <div className="imgWrapper">
-                                            <img src="https://klbtheme.com/bacola/wp-content/uploads/2021/04/product-image-3-346x310.jpg" className="w-100" alt="Product item 2" />
+                                            <img src="https://klbtheme.com/bacola/wp-content/uploads/2021/04/product-image-3-346x310.jpg" className="w-100" alt="Product item 1" />
                                         </div>   
                                     </div>
+                                </SwiperSlide>
+                                <SwiperSlide>
                                     <div className="item productItem">   
                                         <div className="imgWrapper">
-                                            <img src="https://klbtheme.com/bacola/wp-content/uploads/2021/04/product-image-3-346x310.jpg" className="w-100" alt="Product item 3" />
+                                            <img src="https://klbtheme.com/bacola/wp-content/uploads/2021/04/product-image-3-346x310.jpg" className="w-100" alt="Product item 1" />
                                         </div>   
                                     </div>
+                                </SwiperSlide>
+                                <SwiperSlide>
                                     <div className="item productItem">   
                                         <div className="imgWrapper">
-                                            <img src="https://klbtheme.com/bacola/wp-content/uploads/2021/04/product-image-3-346x310.jpg" className="w-100" alt="Product item 4" />
+                                            <img src="https://klbtheme.com/bacola/wp-content/uploads/2021/04/product-image-3-346x310.jpg" className="w-100" alt="Product item 1" />
                                         </div>   
                                     </div>
-                                    <div className="item productItem">   
-                                        <div className="imgWrapper">
-                                            <img src="https://klbtheme.com/bacola/wp-content/uploads/2021/04/product-image-3-346x310.jpg" className="w-100" alt="Product item 5" />
-                                        </div>   
-                                    </div>
-                                </Slider>  
-                            </div>
+                                </SwiperSlide>
+                            </Swiper>  
                         </div>
                     </div>
                 </div>
-            </section>
-        </>
-    )
+            </div>
+        </section>
+    </>
+)
 }
 
 export default Home;
