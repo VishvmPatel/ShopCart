6bf11c34398baa970e3e219b6421482d91766ea1
Vishvam <vishusp77@gmail.com>
2025-03-23 22:54:13 +0530

    update

--- src/Components/HomeBanner/index.js
+++ src/Components/HomeBanner/index.js
@@ -1,5 +1,40 @@
 import React from "react";
 import Slider from "react-slick";
+import image1 from '../../assets/images/image-1.png';
+import image2 from '../../assets/images/image-2.png';
+import image3 from '../../assets/images/image-3.png';
+import image4 from '../../assets/images/image-4.png';
+
+const banners = [
+    {
+        img: image1,
+        title: "A different kind of grocery store",
+        discount: "-20% OFF",
+        price: "$7.99",
+        btnText: "Shop Now"
+    },
+    {
+        img: image2,
+        title: "Quality Freshness Guaranteed!",
+        discount: "-40% OFF",
+        price: "$14.35",
+        btnText: "Shop Now"
+    },
+    {
+        img: image3,
+        title: "Quality Freshness Guaranteed!",
+        discount: "-40% OFF",
+        price: "$14.35",
+        btnText: "Shop Now"
+    },
+    {
+        img: image4,
+        title: "Quality Freshness Guaranteed!",
+        discount: "-40% OFF",
+        price: "$14.35",
+        btnText: "Shop Now"
+    }
+];
 
 const HomeBanner = () => {
     var settings = {
@@ -11,40 +46,25 @@ const HomeBanner = () => {
         arrows: true,
         autoplay: true
     };
-    return(
+
+    return (
         <div className="homeBannerSection">
             <Slider {...settings}>
-                <div className="item">
-                    <img 
-                        src="https://cmsimages.shoppersstop.com/SS_25_main_kv_web_fd8e548010/SS_25_main_kv_web_fd8e548010.png" 
-                        className="w-100"
-                        alt="Shopper's Stop Banner 1"
-                    />
-                </div>
-                <div className="item">
-                    <img 
-                        src="https://cmsimages.shoppersstop.com/SS_25_pvt_brands_main_kv_web_e55ed30937/SS_25_pvt_brands_main_kv_web_e55ed30937.png" 
-                        className="w-100"
-                        alt="Shopper's Stop Private Brands Banner"
-                    />
-                </div>
-                <div className="item">
-                    <img 
-                        src="https://cmsimages.shoppersstop.com/WW_Main_Banners_web_98669c5889/WW_Main_Banners_web_98669c5889.png" 
-                        className="w-100"
-                        alt="Women's Wear Banner"
-                    />
-                </div>
-                <div className="item">
-                    <img 
-                        src="https://cmsimages.shoppersstop.com/Women_Indian_Main_Banners_web_3466e7b36d/Women_Indian_Main_Banners_web_3466e7b36d.png" 
-                        className="w-100"
-                        alt="Women's Indian Wear Banner"
-                    />
-                </div>
-            </Slider>  
-        </div>  
-    )
-}
+                {banners.map((banner, index) => (
+                    <div className="bannerSlide" key={index}>
+                        <img src={banner.img} className="bannerImage" alt={banner.title} />
+                        <div className="bannerContent">
+                            <span className="offerText">{banner.discount}</span>
+                            <h2>{banner.title}</h2>
+                            <p>Only this week. Don’t miss...</p>
+                            <span className="priceText">from <strong>{banner.price}</strong></span>
+                            <button className="shopButton">{banner.btnText} →</button>
+                        </div>
+                    </div>
+                ))}
+            </Slider>
+        </div>
+    );
+};
 
 export default HomeBanner;
