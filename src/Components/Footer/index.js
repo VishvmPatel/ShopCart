55d3918b259704837e4384dff8d2bc47fdd22b36
Vishvam <vishusp77@gmail.com>
2025-05-08 20:14:43 +0530

    update

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,11 +1,33 @@
+import { LuShirt } from "react-icons/lu";
+import { TbTruckDelivery } from "react-icons/tb";
+import { TbDiscount } from "react-icons/tb";
+import { CiBadgeDollar } from "react-icons/ci";
+
 const Footer = ()=>{
     return(
         <footer>
             <div className="container">
                 <div className="topInfo row">
-                    <div className="col">
-                        
+                    <div className="col d-flex align-items-center">
+                        <span><LuShirt/></span>
+                        <span className="ml-2">Everyday fresh products</span>
                     </div>
+
+                    <div className="col d-flex align-items-center">
+                        <span><TbTruckDelivery/></span>
+                        <span className="ml-2">Free delivery for order over $70</span>
+                    </div>
+
+                    <div className="col d-flex align-items-center">
+                        <span><TbDiscount/></span>
+                        <span className="ml-2">Daily Mega Discounts</span>
+                    </div>
+
+                    <div className="col d-flex align-items-center">
+                        <span><CiBadgeDollar/></span>
+                        <span className="ml-2">Best price on the market</span>
+                    </div>
+
                 </div>
             </div>
         </footer>
