41bc01a68088d7406c1d3f3e016ccfb792398852
Vishvam <vishusp77@gmail.com>
2025-05-09 11:25:47 +0530

    update

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -2,6 +2,7 @@ import { LuShirt } from "react-icons/lu";
 import { TbTruckDelivery } from "react-icons/tb";
 import { TbDiscount } from "react-icons/tb";
 import { CiBadgeDollar } from "react-icons/ci";
+import { Link } from "react-router-dom";
 
 const Footer = ()=>{
     return(
@@ -28,6 +29,69 @@ const Footer = ()=>{
                         <span className="ml-2">Best price on the market</span>
                     </div>
 
+                </div>
+
+                <div className="row mt-4 linksWrap">
+                    <div className="col">
+                        <h5>FRUIT & VEGETABLES</h5>
+                        <ul>
+                            <li><Link to='#'>Fresh Vegetables</Link></li>
+                            <li><Link to='#'>Herbs & Seasonings</Link></li>
+                            <li><Link to='#'>Fresh Fruits</Link></li>
+                            <li><Link to='#'>Cuts & Sprouts</Link></li>
+                            <li><Link to='#'>Exotic Fruits & Veggeis</Link></li>
+                            <li><Link to='#'>Packaged Produce</Link></li>
+                            <li><Link to='#'>Party Trays</Link></li>
+                        </ul>
+                    </div>
+                    <div className="col">
+                        <h5>BREAKFAST & DAIRY</h5>
+                        <ul>
+                            <li><Link to='#'>Fresh Vegetables</Link></li>
+                            <li><Link to='#'>Herbs & Seasonings</Link></li>
+                            <li><Link to='#'>Fresh Fruits</Link></li>
+                            <li><Link to='#'>Cuts & Sprouts</Link></li>
+                            <li><Link to='#'>Exotic Fruits & Veggeis</Link></li>
+                            <li><Link to='#'>Packaged Produce</Link></li>
+                            <li><Link to='#'>Party Trays</Link></li>
+                        </ul>
+                    </div>
+                    <div className="col">
+                        <h5>MEAT & SEAFOOD</h5>
+                        <ul>
+                            <li><Link to='#'>Fresh Vegetables</Link></li>
+                            <li><Link to='#'>Herbs & Seasonings</Link></li>
+                            <li><Link to='#'>Fresh Fruits</Link></li>
+                            <li><Link to='#'>Cuts & Sprouts</Link></li>
+                            <li><Link to='#'>Exotic Fruits & Veggeis</Link></li>
+                            <li><Link to='#'>Packaged Produce</Link></li>
+                            <li><Link to='#'>Party Trays</Link></li>
+                        </ul>
+                    </div>
+                    <div className="col">
+                        <h5>BEAVERAGES</h5>
+                        <ul>
+                            <li><Link to='#'>Fresh Vegetables</Link></li>
+                            <li><Link to='#'>Herbs & Seasonings</Link></li>
+                            <li><Link to='#'>Fresh Fruits</Link></li>
+                            <li><Link to='#'>Cuts & Sprouts</Link></li>
+                            <li><Link to='#'>Exotic Fruits & Veggeis</Link></li>
+                            <li><Link to='#'>Packaged Produce</Link></li>
+                            <li><Link to='#'>Party Trays</Link></li>
+                        </ul>
+                    </div>
+                    <div className="col">
+                        <h5>BREADS & BAKERY</h5>
+                        <ul>
+                            <li><Link to='#'>Fresh Vegetables</Link></li>
+                            <li><Link to='#'>Herbs & Seasonings</Link></li>
+                            <li><Link to='#'>Fresh Fruits</Link></li>
+                            <li><Link to='#'>Cuts & Sprouts</Link></li>
+                            <li><Link to='#'>Exotic Fruits & Veggeis</Link></li>
+                            <li><Link to='#'>Packaged Produce</Link></li>
+                            <li><Link to='#'>Party Trays</Link></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </footer>
