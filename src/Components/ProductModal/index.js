35347c37dd1d9c999cbd1c141605c48d2c6be055
Vishvam <vishusp77@gmail.com>
2025-05-10 10:40:40 +0530

    update

--- src/Components/ProductModal/index.js
+++ src/Components/ProductModal/index.js
@@ -1,6 +1,13 @@
+import Dialog from '@mui/material/Dialog';
+import { MdClose } from 'react-icons/md';
+import Button from '@mui/material/Button';
 const ProductModal = ()=>{
     return(
-
+        <>
+            <Dialog open={true}>
+                <Button className='close_'><MdClose /></Button>
+            </Dialog>
+        </>
     )
 }
 
