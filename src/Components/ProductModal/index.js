6838a303a54da2330568b3dea5ac60c0146ed96e
Vishvam <vishusp77@gmail.com>
2025-05-13 00:01:52 +0530

    update

--- src/Components/ProductModal/index.js
+++ src/Components/ProductModal/index.js
@@ -1,12 +1,20 @@
 import Dialog from '@mui/material/Dialog';
 import { MdClose } from 'react-icons/md';
 import Button from '@mui/material/Button';
+import { Rating } from '@mui/material';
 const ProductModal = (props)=>{
     return(
         <>
-            <Dialog open={true} onClose={()=>props.closeProductModal()}>
-                <Button className='close_'><MdClose /></Button>
-                <h4 class = "mb-0">All Natural Italian-Style Chicken Meatballs</h4>
+            <Dialog open={true} className='productModal' onClose={()=>props.closeProductModal()}>
+                <Button className='close_' onClick={()=>props.closeProductModal()}><MdClose /></Button>
+                <h4 class = "mb-1 font-weight-bold">All Natural Italian-Style Chicken Meatballs</h4>
+                <div className='d-flex align-items-center'>
+                    <div className='d-flex align-items-center mr-4'>
+                        <span>Brands:</span>
+                        <span className='ml-2'>Welch's</span>
+                    </div>
+                    <Rating name='read-only' value={5} size='small' precision={0.5} readOnly />
+                </div>
             </Dialog>
         </>
     )
