44980bc381a03cbf76a4c7b66d38dded3b857b5a
Vishvam <vishusp77@gmail.com>
2025-05-13 13:33:37 +0530

    update

--- src/Components/ProductModal/index.js
+++ src/Components/ProductModal/index.js
@@ -11,10 +11,14 @@ const ProductModal = (props)=>{
                 <div className='d-flex align-items-center'>
                     <div className='d-flex align-items-center mr-4'>
                         <span>Brands:</span>
-                        <span className='ml-2'>Welch's</span>
+                        <span className='ml-2'><b>Welch's</b></span>
                     </div>
                     <Rating name='read-only' value={5} size='small' precision={0.5} readOnly />
+                            
                 </div>
+                <br></br>
+                <br></br>
+                <br></br>
             </Dialog>
         </>
     )
