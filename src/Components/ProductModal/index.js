88fe61516f796fcc5933f24880d9ca5c23550950
Vishvam <vishusp77@gmail.com>
2025-05-16 17:14:07 +0530

    update

--- src/Components/ProductModal/index.js
+++ src/Components/ProductModal/index.js
@@ -5,6 +5,7 @@ import { Rating } from '@mui/material';
 import Slider from 'react-slick';
 import productImage1 from "../../assets/images/product-image-62.jpg";
 import productImage2 from "../../assets/images/product-image2-47.jpg";
+import productImage3 from "../../assets/images/product-image3-35.jpg";
 import { useRef } from 'react';
 import InnerImageZoom from 'react-inner-image-zoom';
 import 'react-inner-image-zoom/lib/styles.min.css';
@@ -57,16 +58,24 @@ const ProductModal = (props)=>{
                             <Slider {...settings2} className='zoomSliderBig' ref={zoomSliderBig}>
                                 <div className='item'>
                                     <InnerImageZoom src={productImage1} zoomType="hover" zoomScale={1} />
-
+                                </div>
+                                <div className='item'>
+                                    <InnerImageZoom src={productImage2} zoomType="hover" zoomScale={1} />
+                                </div>
+                                <div className='item'>
+                                    <InnerImageZoom src={productImage3} zoomType="hover" zoomScale={1} />
                                 </div>
                             </Slider>
                         </div>
                         <Slider {...settings} className='zoomSlider' ref={zoomSlider}>
                             <div className='item'>
-                                <img src={productImage1} className='w-100' onClick={()=> goto(0)}/>
+                                <img src={productImage1} alt="ProductI1" className='w-100' onClick={() => goto(0)} />
                             </div>
                             <div className='item'>
-                                <img src={productImage2} className='w-100' onClick={()=> goto(1)}/>
+                                <img src={productImage2} alt="ProductI2" className='w-100' onClick={() => goto(1)} />
+                            </div>
+                            <div className='item'>
+                                <img src={productImage3} alt="ProductI3" className='w-100' onClick={() => goto(2)} />
                             </div>
                         </Slider>
                     </div>
