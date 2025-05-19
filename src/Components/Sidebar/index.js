0afd372f925c93593230ca06b15997153d6ec94a
Vishvam <vishusp77@gmail.com>
2025-05-19 21:49:56 +0530

    update

--- src/Components/Sidebar/index.js
+++ src/Components/Sidebar/index.js
@@ -1,8 +1,26 @@
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import CheckBox from "@mui/material/Checkbox";
+
 const Sidebar = () =>{
     return(
         <>
         <div className="sidebar">
-            
+            <div className="filterBox">
+                <h6>PRODUCT CATEGORIES</h6>
+
+                <div className="scroll">
+                    <ul>
+                        <li>
+                            <FormControlLabel className="w-100" control={<CheckBox />} label="Men" />
+                        </li>
+                        <li>
+                            <FormControlLabel className="w-100" control={<CheckBox />} label="Men" />
+                        </li>
+                    
+                    </ul>
+                </div>
+            </div>
         </div>
         </>
     )
