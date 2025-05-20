7621ec970dcbbc922ee9bc6d2361033b84a4d27e
Vishvam <vishusp77@gmail.com>
2025-05-20 12:14:23 +0530

    update

--- src/Pages/Listing/index.js
+++ src/Pages/Listing/index.js
@@ -3,11 +3,23 @@ import content1 from"../../assets/images/content1.jpg";
 import Button from '@mui/material/Button';
 import {IoIosMenu} from "react-icons/io";
 import {CgMenuGridR} from "react-icons/cg";
-import {TbGridDots} from "react-icons/tb";
 import {HiViewGrid} from "react-icons/hi";
 import {TfiLayoutGrid4Alt} from "react-icons/tfi";
+import {FaAngleDown} from "react-icons/fa6";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { useState } from "react";
 
 const Listing = () => {
+    const [anchorEl , setAnchorEl] = useState(null);
+    const openDropDown = Boolean(anchorEl);
+    const handleClick = (event) =>{
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
     return(
         <>
             <section className="product_Listing_Page">
@@ -20,12 +32,30 @@ const Listing = () => {
 
                             <div className="showBy mt-3 mb-3 d-flex align-items-center">
                                 <div className="d-flex align-items-center btnWrapper">
-                                    <Button><IoIosMenu/></Button>
-                                    <Button><HiViewGrid/></Button>
-                                    <Button><TfiLayoutGrid4Alt/></Button>
-                                    <Button><CgMenuGridR/></Button>
+                                    <Button><IoIosMenu /></Button>
+                                    <Button><HiViewGrid /></Button>
+                                    <Button><TfiLayoutGrid4Alt /></Button>
+                                    <Button><CgMenuGridR /></Button>
+                                </div>
+
+                                <div className="showByFilter" style={{ marginLeft: 'auto' }}>
+                                    <Button onClick={handleClick}>Show 9 <FaAngleDown /></Button>
+                                    <Menu
+                                        id="basic-menu"
+                                        anchorEl={anchorEl}
+                                        open={openDropDown}
+                                        onClose={handleClose}
+                                        MenuListProps={{ 'aria-labelledby': 'basic-button' }}
+                                    >
+                                        <MenuItem onClick={handleClose}>10</MenuItem>
+                                        <MenuItem onClick={handleClose}>20</MenuItem>
+                                        <MenuItem onClick={handleClose}>30</MenuItem>
+                                        <MenuItem onClick={handleClose}>40</MenuItem>
+                                        <MenuItem onClick={handleClose}>50</MenuItem>
+                                    </Menu>
                                 </div>
                             </div>
+
                         </div>
                     </div>
                 </div>
