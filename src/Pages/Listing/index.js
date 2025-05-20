d67055219a956c34c66cce418d158b7000873dac
Vishvam <vishusp77@gmail.com>
2025-05-20 11:17:59 +0530

    update

--- src/Pages/Listing/index.js
+++ src/Pages/Listing/index.js
@@ -1,5 +1,11 @@
 import Sidebar from "../../Components/Sidebar";
 import content1 from"../../assets/images/content1.jpg";
+import Button from '@mui/material/Button';
+import {IoIosMenu} from "react-icons/io";
+import {CgMenuGridR} from "react-icons/cg";
+import {TbGridDots} from "react-icons/tb";
+import {HiViewGrid} from "react-icons/hi";
+import {TfiLayoutGrid4Alt} from "react-icons/tfi";
 
 const Listing = () => {
     return(
@@ -11,6 +17,15 @@ const Listing = () => {
 
                         <div className="content_right">
                             <img src={content1} className="w-100" style={{borderRadius:'8px'}}/>
+
+                            <div className="showBy mt-3 mb-3 d-flex align-items-center">
+                                <div className="d-flex align-items-center btnWrapper">
+                                    <Button><IoIosMenu/></Button>
+                                    <Button><HiViewGrid/></Button>
+                                    <Button><TfiLayoutGrid4Alt/></Button>
+                                    <Button><CgMenuGridR/></Button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
