a7472354f32449829743d47ce8f236b188f5e977
Vishvam <vishusp77@gmail.com>
2025-05-20 23:22:27 +0530

    update

--- src/Components/ProductModal/index.js
+++ src/Components/ProductModal/index.js
@@ -2,52 +2,20 @@ import Dialog from '@mui/material/Dialog';
 import { MdClose, MdOutlineCompareArrows } from 'react-icons/md';
 import Button from '@mui/material/Button';
 import { Rating } from '@mui/material';
-import Slider from 'react-slick';
-import productImage1 from "../../assets/images/product-image-62.jpg";
-import productImage2 from "../../assets/images/product-image2-47.jpg";
-import productImage3 from "../../assets/images/product-image3-35.jpg";
 import { useContext, useRef } from 'react';
-import InnerImageZoom from 'react-inner-image-zoom';
-import 'react-inner-image-zoom/lib/styles.min.css';
 import QuantityBox from '../QuantityBox';
 import { IoIosHeartEmpty } from 'react-icons/io';
 import { MyContext } from '../../App';
+import ProductZoom from '../ProductZoom';
 
 const ProductModal = (props)=>{
     
-    const zoomSliderBig = useRef();
-    const zoomSlider = useRef();
-
     const context = useContext(MyContext);
     const handleClose = () => {
         context.setisOpenProductModal(false);
         document.body.classList.remove("modal-open");
     };
 
-
-    var settings = {
-        dots:false,
-        infinite: false,
-        speed: 500,
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        fade: false,
-        arrows: true,
-    };
-    var settings2 = {
-        dots: false,
-        infinite: false,
-        speed: 700,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        fade: false,
-        arrows: false,
-    };
-
-    const goto = (index) =>{
-        zoomSlider.current.slickGoTo(index);
-        zoomSliderBig.current.slickGoTo(index);
-    }
     return(
         <>
             <Dialog open={true} className='productModal' onClose={handleClose}>
@@ -65,31 +33,7 @@ const ProductModal = (props)=>{
 
                 <div className='row mt-2 productDetailModal'>
                     <div className='col-md-5'>
-                        <div className='productZoom position-relative'>
-                            <div className='badge badge-primary'>23%</div>
-                            <Slider {...settings2} className='zoomSliderBig' ref={zoomSliderBig}>
-                                <div className='item'>
-                                    <InnerImageZoom src={productImage1} zoomType="hover" zoomScale={1} />
-                                </div>
-                                <div className='item'>
-                                    <InnerImageZoom src={productImage2} zoomType="hover" zoomScale={1} />
-                                </div>
-                                <div className='item'>
-                                    <InnerImageZoom src={productImage3} zoomType="hover" zoomScale={1} />
-                                </div>
-                            </Slider>
-                        </div>
-                        <Slider {...settings} className='zoomSlider' ref={zoomSlider}>
-                            <div className='item'>
-                                <img src={productImage1} alt="ProductI1" className='w-100' onClick={() => goto(0)} />
-                            </div>
-                            <div className='item'>
-                                <img src={productImage2} alt="ProductI2" className='w-100' onClick={() => goto(1)} />
-                            </div>
-                            <div className='item'>
-                                <img src={productImage3} alt="ProductI3" className='w-100' onClick={() => goto(2)} />
-                            </div>
-                        </Slider>
+                        <ProductZoom/>
                     </div>
                     <div className='col-md-7'>
                         <div className='d-flex info align-items-center mb-4'>
