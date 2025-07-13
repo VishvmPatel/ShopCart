9b37b033bed577d85e07845a796e456a2f84317b
Vishvam <vishusp77@gmail.com>
2025-07-13 14:39:16 +0530

    update

--- src/Pages/Cart/index.js
+++ src/Pages/Cart/index.js
@@ -4,7 +4,8 @@ const Cart = () => {
         <>
             <section className="section">
                 <div className="container">
-                    <h2 className="hd">Cart</h2>
+                    <h2 className="hd mb-0">Your Cart</h2>
+                    <p>There are <b>3</b> products in your cart.</p>
                 </div>
             </section>
         </>
